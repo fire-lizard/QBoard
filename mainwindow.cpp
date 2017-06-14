19e2164a6d09f59ed0cccf2e72fb1887d105244e
Anatoliy Sova <sovaar@hotmail.com>
2017-06-14 22:40:05 +1000

    images

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -30,5 +30,79 @@ void MainWindow::on_actionAbout_triggered()
 
 void MainWindow::on_actionExit_triggered()
 {
-    QApplication::quit();
+    exit(0);
+}
+
+void MainWindow::paintEvent(QPaintEvent *evt)
+{
+    QPainter painter(ui->vboard);
+        painter.setPen(Qt::black);
+        painter.setBrush(Qt::NoBrush);
+
+        QSize s = ui->vboard->size();;
+        int w = s.width() / _board.GetWidth();
+        int h = s.height() / _board.GetHeight();
+        for (int i = 0; i < _board.GetWidth(); i++)
+        {
+            for (int j = 0; j < _board.GetHeight(); j++)
+            {
+                /*if (PossibleMove(i, j))
+                {
+                    if (_board.GetData(i, j) != nullptr)
+                    {
+                        dc.SetBrush(*wxRED_BRUSH);
+                        dc.DrawRectangle(i * w, j * h, w, h);
+                        dc.SetBrush(wxNullBrush);
+                    }
+                    else if (_board.GetData(i, j) == nullptr)
+                    {
+                        dc.SetBrush(*wxCYAN_BRUSH);
+                        dc.DrawRectangle(i * w, j * h, w, h);
+                        dc.SetBrush(wxNullBrush);
+                    }
+                }
+                else if (any_of(_opponentMoves.begin(), _opponentMoves.end(), [=](pair<int,int> p){return p.first == i && p.second == j;}))
+                {
+                    if (_board.GetData(i, j) != nullptr && (_board.GetData(i, j)->GetType() == King || _board.GetData(i, j)->GetType() == Queen))
+                    {
+                        dc.SetBrush(*wxYELLOW_BRUSH);
+                        if (_board.GetData(i, j)->GetType() == King)
+                            _check = true;
+                    }
+                    dc.DrawRectangle(i * w, j * h, w, h);
+                    dc.SetBrush(wxNullBrush);
+                }
+                else
+                {
+                    dc.DrawRectangle(i * w, j * h, w, h);
+                }*/
+            }
+        }
+        /*for (int i = 0; i < _board.GetWidth(); i++)
+        {
+            for (int j = 0; j < _board.GetHeight(); j++)
+            {
+                Piece *p = _board.GetData(i, j);
+                if (p != nullptr)
+                {
+                    PieceType t = p->GetType();
+                    PieceColour c = p->GetColour();
+                    wxImage image("images/" + Piece::GetImageFileName(t, c));
+                    wxBitmap bitmap(image);
+                    dc.DrawBitmap(bitmap, i * w + 10, j * h + 5);
+                }
+            }
+        }*/
+}
+
+bool MainWindow::PossibleMove(int x, int y)
+{
+    for (unsigned index = 0; index < _moves.size(); index++)
+    {
+        if (_moves[index].first == x && _moves[index].second == y)
+        {
+            return true;
+        }
+    }
+    return false;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,7 +3,10 @@
 
 #include <QMainWindow>
 #include <QMessageBox>
+#include <QPainter>
 #include "settingsdialog.h"
+#include "ChuShogiBoard.h"
+#include "ChessBoard.h"
 
 namespace Ui {
 class MainWindow;
@@ -24,8 +27,23 @@ private slots:
 
     void on_actionExit_triggered();
 
+    void paintEvent(QPaintEvent *evt);
+
 private:
     Ui::MainWindow *ui;
+
+    bool PossibleMove(int x, int y);
+
+    //ChuShogiBoard _board;
+    ChessBoard _board;
+    PieceColour _currentPlayer = White;
+    bool _check = false;
+    Piece *_currentPiece;
+    int _oldX = -1;
+    int _oldY = -1;
+    vector<pair<int,int>> _moves;
+    vector<pair<int,int>> _myMoves;
+    vector<pair<int,int>> _opponentMoves;
 };
 
 #endif // MAINWINDOW_H
