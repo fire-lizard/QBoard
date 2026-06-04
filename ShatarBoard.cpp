e112481506482a02d3326251744fc7b45913aeba
fire-waran <141347636+fire-waran@users.noreply.github.com>
2026-06-04 16:43:29 +0300

    1.1

--- ShatarBoard.cpp
+++ ShatarBoard.cpp
@@ -13,17 +13,17 @@ ShatarBoard::~ShatarBoard()
 
 Board* ShatarBoard::Clone()
 {
-    ShatarBoard* cb = new ShatarBoard();
+    ShatarBoard *sb = new ShatarBoard();
     for (int i = 0; i < GetWidth(); i++)
     {
         for (int j = 0; j < GetHeight(); j++)
         {
             const std::optional<Piece> p = GetData(i, j);
-            cb->SetData(i, j, p != std::nullopt ? std::make_optional<Piece>(p->Type, p->Colour) : std::nullopt);
+            sb->SetData(i, j, p != std::nullopt ? std::make_optional<Piece>(p->Type, p->Colour) : std::nullopt);
         }
     }
-    cb->SetMoveCount(_moveCount);
-    return cb;
+    sb->SetMoveCount(_moveCount);
+    return sb;
 }
 
 void ShatarBoard::Initialize()
@@ -42,6 +42,66 @@ void ShatarBoard::Initialize()
             {
                 SetData(i, j, std::nullopt);
             }
+        }
+    }
+}
+
+void ShatarBoard::SetEvalMode(bool evalMode)
+{
+    _evalMode = evalMode;
+}
+
+bool ShatarBoard::IsCheckmate(int oldX, int oldY, int newX, int newY)
+{
+    ShatarBoard *sb = dynamic_cast<ShatarBoard*>(Clone());
+    sb->SetEvalMode(true);
+    auto playerMoves = sb->GetAllMoves(GetData(newX, newY)->Colour == White ? Black : White);
+    auto opponentMoves = sb->GetAllMoves(GetData(newX, newY)->Colour);
+    auto cnt = opponentMoves.size();
+	for (auto& opponentMove : opponentMoves)
+    {
+        if (std::get<0>(opponentMove) == newX && std::get<1>(opponentMove) == newY)
+        {
+            for (auto& playerMove : playerMoves)
+            {
+                if (std::get<2>(playerMove) == std::get<2>(opponentMove) && std::get<3>(playerMove) == std::get<3>(opponentMove))
+                {
+                    cnt--;
+                }
+            }
+        }
+        else if (std::get<2>(opponentMove) == oldX && std::get<3>(opponentMove) == oldY)
+        {
+            return false;
+        }
+        else
+        {
+            cnt--;
+        }
+    }
+    delete sb;
+    return cnt <= 0;
+}
+
+void ShatarBoard::CheckKnightMove(const std::optional<Piece>& piece, int x, int y)
+{
+    CheckMove(piece, x, y);
+	std::vector<std::pair<int, int>> knightMoves;
+    knightMoves.emplace_back(x + 1, y + 2);
+    knightMoves.emplace_back(x - 1, y + 2);
+    knightMoves.emplace_back(x + 2, y + 1);
+    knightMoves.emplace_back(x + 2, y - 1);
+    knightMoves.emplace_back(x - 2, y + 1);
+    knightMoves.emplace_back(x - 2, y - 1);
+    knightMoves.emplace_back(x + 1, y - 2);
+    knightMoves.emplace_back(x - 1, y - 2);
+    for (auto& knightMove : knightMoves)
+    {
+        auto destPiece = GetData(knightMove.first, knightMove.second);
+        if (destPiece != std::nullopt && destPiece->Colour != piece->Colour && destPiece->Type == King && IsCheckmate(x, y, knightMove.first, knightMove.second))
+        {
+            RemoveMove(x, y);
+            break;
         }
     }
 }
@@ -67,6 +127,21 @@ void ShatarBoard::GetMoves(const std::optional<Piece>& piece, int x, int y)
         CheckDirection(piece, x, y, SouthEast);
         CheckDirection(piece, x, y, SouthWest);
         break;
+    case Knight:
+        // Knight cannot be checkmate
+        if (!_evalMode)
+        {
+            CheckKnightMove(piece, x + 1, y + 2);
+            CheckKnightMove(piece, x - 1, y + 2);
+            CheckKnightMove(piece, x + 2, y + 1);
+            CheckKnightMove(piece, x + 2, y - 1);
+            CheckKnightMove(piece, x - 2, y + 1);
+            CheckKnightMove(piece, x - 2, y - 1);
+            CheckKnightMove(piece, x + 1, y - 2);
+            CheckKnightMove(piece, x - 1, y - 2);
+        }
+        else ShatranjBoard::GetMoves(piece, x, y);
+		break;
     case Pawn:
         if (piece->Colour == Black)
         {
--- ShatarBoard.h
+++ ShatarBoard.h
@@ -9,8 +9,13 @@ public:
     void Initialize() override;
     Board* Clone() override;
     void GetMoves(const std::optional<Piece>& piece, int x, int y) override;
+    void SetEvalMode(bool evalMode);
 
 private:
+    void CheckKnightMove(const std::optional<Piece>& piece, int x, int y);
+    bool IsCheckmate(int oldX, int oldY, int newX, int newY);
+
+    bool _evalMode = false;
 
     PieceType _initialSetup[8][8] = {
         { Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook },
